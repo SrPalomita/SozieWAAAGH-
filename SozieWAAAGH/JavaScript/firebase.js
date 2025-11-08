8eb27cb3979faef736c2613ff90a37b9ea35f1b5
SrPalomita <joannicolasfogleaaldana@gmail.com>
2025-11-08 18:42:01 -0500

    Update firebase.js

--- SozieWAAAGH/JavaScript/firebase.js
+++ SozieWAAAGH/JavaScript/firebase.js
@@ -1,6 +1,7 @@
 // firebase.js (MODULAR v10)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBZJ9F7ZqxGcXJ1MxduQ_2KeobguPIE0",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
